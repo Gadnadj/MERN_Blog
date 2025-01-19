8efe90b8b0ff1959b65f52cb97aa49333035f35c
Gad Nadjar <gadnadjar@gmail.com>
2025-01-19 13:37:57 +0200

    connect database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+console.log(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => { console.log('Database connected'); })
+    .catch((err) => { console.log(err); });
 const app = express();
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
 });
+
+//C97Gnyt9cadMxPvE
+//gadnadjar
