373371968424a7092d492637cc63e35997302396
Gad Nadjar <gadnadjar@gmail.com>
2025-01-19 15:48:33 +0200

    add sign in api route

--- api/index.js
+++ api/index.js
@@ -4,21 +4,21 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
+
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => { console.log('Database connected'); })
     .catch((err) => { console.log(err); });
+
 const app = express();
+
+app.use(express.json());
+
 
 app.listen(4000, () => {
     console.log('listening on port 4000');
 });
 
-app.get('/test', (req, res) => {
-    res.json({ message: 'API is working!' });
-})
 
-//middlewares
-app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
