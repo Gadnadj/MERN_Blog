33b346a4abd8fe9d0e7c77834cd0ba975137b559
Gad Nadjar <gadnadjar@gmail.com>
2025-01-19 13:19:35 +0200

    install rect js and tailwind css and create files

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+import Headers from './components/Headers';
+
+function App() {
+
+
+  return (
+
+    <BrowserRouter>
+      <Headers />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
