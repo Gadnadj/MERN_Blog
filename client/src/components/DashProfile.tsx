3bd7e963e99447377905eefc1168ee89d5923751
Gad Nadjar <gadnadjar@gmail.com>
2025-01-20 15:28:17 +0200

    add sidebar dashboard

---
+++ client/src/components/DashProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {}
+
+const DashProfile = (props: Props) => {
+    return (
+        <div>DashProfile</div>
+    );
+};
+
+export default DashProfile;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 type Props = {}
 
 const Dashboard = (props: Props) => {
+    const location = useLocation();
+    const [tab, setTab] = useState<string>('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        console.log(tabFromUrl);
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location]);
+
     return (
-        <div className='h-screen'>Dashboard</div>
+        <div className='h-screen flex flex-col md:flex-row'>
+            {/* Sidebar */}
+            <div className='md:w-56'>
+                <DashSidebar />
+            </div>
+
+            {/* Profile... */}
+            {tab === 'profile' && <DashProfile />}
+        </div>
     );
 };
 
