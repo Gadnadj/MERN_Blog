aec75947c73481b804cf692319afc8aad50f36f4
Gad Nadjar <gadnadjar@gmail.com>
2025-01-20 09:10:10 +0200

    add new reducer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { store } from './redux/store.ts';
+import { store, persistor } from './redux/store.ts';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.tsx';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 
 );
